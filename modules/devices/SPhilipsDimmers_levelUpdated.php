37f7a4d6ad7be7ed1a3c2c4c2dc45265a9a8840c
Alexandr-logrus <nechaev.alex@gmail.com>
2018-10-03 15:53:02 +0500

    fix cross-linked

--- modules/devices/SPhilipsDimmers_levelUpdated.php
+++ modules/devices/SPhilipsDimmers_levelUpdated.php
@@ -2,7 +2,7 @@
 
 if ($this->getProperty('level')>0) {
  $this->setProperty('saved_level', $this->getProperty('level'));
- $this->setProperty('cct', $this->getProperty('saved_cct'));
+ //$this->setProperty('cct', $this->getProperty('saved_cct'));
  $this->setProperty('status', 1);
 } else {
  $this->setProperty('status', 0);
