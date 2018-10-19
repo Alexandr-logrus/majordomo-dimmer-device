bf8954f0ec80861e609a50c14e3f07dbc5806f56
Alexandr-logrus <nechaev.alex@gmail.com>
2018-10-19 12:34:00 +0500

    fix
    1. поправил описание структуры и переименовал (удалить вручную)
    2. удалил brightWorkUpdated

--- modules/devices/addons/SPhilipsDimmers_structure.php
+++ modules/devices/addons/SPhilipsDimmers_structure.php
@@ -1,21 +1,17 @@
 <?php
 
-$this->device_types['dimmer'] = array(
-    'TITLE'=>"Philips Zhirui Downlight",
+$this->device_types['dimmerph'] = array(
+    'TITLE'=>"Philips light",
     'PARENT_CLASS'=>'SControllers',
     'CLASS'=>'SPhilipsDimmers',
     'PROPERTIES'=>array(
         'bright'=>array('DESCRIPTION'=>'Current brightness level','ONCHANGE'=>'brightUpdated','DATA_KEY'=>1),
         'saved_bright'=>array('DESCRIPTION'=>''),
-        'brightWork'=>array('DESCRIPTION'=>'Brightness level','ONCHANGE'=>'brightWorkUpdated'),
-        'minWork'=>array('DESCRIPTION'=>'Min brigh','_CONFIG_TYPE'=>'num','_CONFIG_HELP'=>'SdDimmerMinMax'),
-        'maxWork'=>array('DESCRIPTION'=>'Max brigh','_CONFIG_TYPE'=>'num','_CONFIG_HELP'=>'SdDimmerMinMax'),
         'cct'=>array('DESCRIPTION'=>'Цветовая температура','ONCHANGE'=>'cctUpdated','DATA_KEY'=>1),
         'online'=>array('DESCRIPTION'=>'В сети','DATA_KEY'=>1)
     ),
     'METHODS'=>array(
         'brightUpdated'=>array('DESCRIPTION'=>'Bright Updated'),
-        'brightWorkUpdated'=>array('DESCRIPTION'=>'Bright Work Updated'),
         'cctUpdated'=>array('DESCRIPTION'=>'Bright Updated'),
         'snmUpdated'=>array('DESCRIPTION'=>''),
         'turnOn'=>array('DESCRIPTION'=>'Dimmer turnOn'),
