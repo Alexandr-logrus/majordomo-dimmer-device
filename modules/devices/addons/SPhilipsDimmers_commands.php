faf7aba3585fc46e70594e526604913a4a8d3c4b
Alexandr-logrus <nechaev.alex@gmail.com>
2019-03-04 01:10:13 +0500

    Update SPhilipsDimmers_commands.php
    незначительные правки

--- modules/devices/addons/SPhilipsDimmers_commands.php
+++ modules/devices/addons/SPhilipsDimmers_commands.php
@@ -3,15 +3,15 @@
 if ($device_type == 'dimmerph') {
  if (preg_match('/' . LANG_DEVICES_PATTERN_TURNON . '/uis', $command)) {
   sayReplySafe(LANG_TURNING_ON . ' ' . $device_title . $add_phrase, 2);
-  $run_code .= "callMethodSafe('$linked_object.turnOn');";
-  $opposite_code .= "callMethodSafe('$linked_object.turnOff');";
+  $run_code .= "callMethod('$linked_object.turnOn');";
+  $opposite_code .= "callMethod('$linked_object.turnOff');";
   $processed = 1;
   //$reply_confirm = 1;
  }
  elseif (preg_match('/' . LANG_DEVICES_PATTERN_TURNOFF . '/uis', $command)) {
   sayReplySafe(LANG_TURNING_OFF . ' ' . $device_title . $add_phrase, 2);
-  $run_code .= "callMethodSafe('$linked_object.turnOff');";
-  $opposite_code .= "callMethodSafe('$linked_object.turnOn');";
+  $run_code .= "callMethod('$linked_object.turnOff');";
+  $opposite_code .= "callMethod('$linked_object.turnOn');";
   $processed = 1;
   //$reply_confirm = 1;
  }
